89573465cf8dd0573caa8777e8f5738971034035
Gabriel Brandão <35882052+bakill3@users.noreply.github.com>
2019-01-26 18:55:23 +0000

    More protected login
    Login more protected against SQL & JS injection

--- ajax/login.php
+++ ajax/login.php
@@ -2,14 +2,14 @@
 include($_SERVER['DOCUMENT_ROOT']."/plootus/head/connect.php");
 
 if(isset($_POST['email_phone']) === true & isset($_POST['password']) === true ){
-	$email_phone = $_POST['email_phone'];
-	$password = $_POST['password'];
+	$email_phone = htmlspecialchars(mysqli_real_escape_string($conStudent, $_POST['email_phone']));
+	$password = htmlspecialchars(mysqli_real_escape_string($conStudent, $_POST['password']));
 
 	//Get username, password, and salt
 	$userquery = mysqli_query($conStudent, "SELECT UID,Email,Password,Salt FROM Students WHERE (Email='$email_phone' OR Phone='$email_phone') AND Closed='0'");
 	$userCount = mysqli_num_rows($userquery);
 
-	if($userCount == 1){ #check if user exists
+	if($userCount > 0){ #check if user exists
 		$row = mysqli_fetch_assoc($userquery);
 		$email = $row["Email"];
 		$uid = $row["UID"];
@@ -22,6 +22,8 @@ if(isset($_POST['email_phone']) === true & isset($_POST['password']) === true ){
 			//set session variables
 			$_SESSION['UID'] = $uid;
 			$_SESSION['Email'] = $email;
+			//header('Location: http://rayyanq.com/plootus/dashboard'); I advise commenting the echo and puting these 2 lines
+			//exit(0); # <-- close header
 			echo"<meta http-equiv=\"refresh\" content=\"0; url=http://rayyanq.com/plootus/dashboard\">";
 		}
 		else
